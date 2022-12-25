c420cf7e5b62521b8e173c95799897c696fd6cca
narutaku0914 <takumi.nalusawa@gmail.com>
2022-12-25 11:45:29 +0900

    Day9+appendix実装

--- work/app.py
+++ work/app.py
@@ -1,41 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
 
-from datetime import time, datetime
 
-st.header('st.slider')
+st.header('Line chart with Altair')
 
-# ex1
-st.subheader('Slider')
+source = pd.DataFrame({
+  'x': np.arange(20),
+  'a': np.random.randn(20),
+})
 
-# title, min, max, initial
-age = st.slider('How old are you?', 20, 90, 30)
-st.write("I'm", age, 'years old')
-
-# ex2
-st.subheader('Range slider')
-
-values = st.slider(
-  'Select a range of values',
-  0.0, 100.0, (25.1, 75.3)
+chart = alt.Chart(source).mark_line().encode(
+  x='x',
+  y='a'
 )
-st.write('Values:', values)
 
-# ex3
-st.subheader('Range time slider')
-appointment = st.slider(
-  "Schedule your appointment:",
-  value=(time(11,30), time(12,45))
-)
-st.write("You're scheduled for:", appointment)
-
-# ex4
-# Example 4
-
-st.subheader('Datetime slider')
-
-start_time = st.slider(
-  "When do you start?",
-  value=datetime(2020, 1, 1, 9, 30),
-  format="MM/DD/YY - hh:mm"
-)
-st.write("Start time:", start_time)
+st.altair_chart(chart, use_container_width=True)
