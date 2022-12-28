d5aa0b33f9f5c421908e095a7cb3cb1cb9fa7555
narutaku0914 <takumi.nalusawa@gmail.com>
2022-12-28 21:03:54 +0900

    Day12修正

--- work/app.py
+++ work/app.py
@@ -2,20 +2,16 @@ import streamlit as st
 
 st.header('st.checkbox')
 
-st.write('What would you like to order?')
-
 # checkboxがcheckedか否かを返す？
-icecream = st.checkbox('Ice cream')
-coffee = st.checkbox('coffee')
-cola = st.checkbox('cola')
+check = st.checkbox('Click here')
 
-if icecream:
-  st.write("Great! Here's some more 🍦")
+if check:
+  st.write(f"State of the checkbox: {check}")
 
-if coffee:
-  st.write("Okey, here's some coffee ☕️")
+check2 = st.checkbox("Uncheck to remove cake", value=True)
 
-if cola:
-  st.write("Here we go 🥤")
+if check2:
+  st.write(f"State of the checkbox: {check2}")
+  st.write(":cake:"*102)
 
 
