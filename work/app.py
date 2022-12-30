350f7e88f93fa7a13503970e9f4aa0b8e3809bf0
narutaku0914 <takumi.nalusawa@gmail.com>
2022-12-30 23:39:53 +0900

    YouTube埋め込み

--- work/app.py
+++ work/app.py
@@ -1,14 +1,30 @@
 import streamlit as st
-import pandas as pd
-import pandas_profiling
 
-from streamlit_pandas_profiling import st_profile_report
+from streamlit_player import st_player, _SUPPORTED_EVENTS
 
-st.header('streamlit_pandas_profiling')
+def main():
+  c1, c2 = st.columns([6, 4])
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  with c2:
+    st.subheader("Parameters")
 
-profile_report = df.profile_report()
-st_profile_report(profile_report)
+    options = {
+        "events": st.multiselect("Events to listen", _SUPPORTED_EVENTS, ["onProgress"]),
+        "progress_interval": 1000,
+        "volume": st.slider("Volume", 0.0, 1.0, 1.0, .01),
+        "playing": st.checkbox("Playing", False),
+        "loop": st.checkbox("Loop", False),
+        "controls": st.checkbox("Controls", True),
+        "muted": st.checkbox("Muted", False),
+    }
 
+  with c1:
+    url = st.text_input("First URL", "https://youtu.be/c9k8K1eII4g")
+    event = st_player(url, **options, key="youtube_player")
+
+    st.write(event)
+
+
+if __name__ == "__main__":
+  main()
 
