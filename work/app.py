1835d8ac899b1cd30224ff48ee24cd43eceedba0
narutaku0914 <takumi.nalusawa@gmail.com>
2023-01-08 22:29:15 +0900

    Day23実装

--- work/app.py
+++ work/app.py
@@ -1,64 +1,30 @@
 import streamlit as st
-import time
 
-st.title('st.form')
+st.title('st.experimental_get_query_params')
 
-st.header('1. Example of using `with` notation')
-st.subheader('Coffee machine')
+with st.expander('About this app'):
+  st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
 
-# with内
-with st.form('my_form'):
-    st.subheader('**Order your coffee**')
-
-    # 選択した項目を変数に格納
-    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
-    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
-    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
-    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
-    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
-    owncup_val = st.checkbox('Bring own cup')
+# 1. Instructions
+st.header('1. Instructions')
+st.markdown('''
+In the above URL bar of your internet browser, append the following:
+`?name=Jack&surname=Beanstalk`
+after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
+such that it becomes 
+`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
+''')
 
 
-    submitted = st.form_submit_button('Submit')
-
-if submitted:
-    my_bar = st.progress(0)
-
-    for percent_complete in range(100):
-        time.sleep(0.05)
-        my_bar.progress(percent_complete + 1)
-        
-    st.markdown(f'''
-        ☕ You have ordered:
-        - Coffee bean: `{coffee_bean_val}`
-        - Coffee roast: `{coffee_roast_val}`
-        - Brewing: `{brewing_val}`
-        - Serving type: `{serving_type_val}`
-        - Milk: `{milk_val}`
-        - Bring own cup: `{owncup_val}`
-        ''')
-else:
-    st.write('☝️ Place your order!')
+# 2. Contents of st.experimental_get_query_params
+st.header('2. Contents of st.experimental_get_query_params')
+st.write(st.experimental_get_query_params())
 
 
-# object
-# st.header('2. Example of object notation')
+# 3. Retrieving and displaying information from the URL
+st.header('3. Retrieving and displaying information from the URL')
 
-# form = st.form('my_form_2')
-# coffee_bean_val = form.selectbox('Coffee bean', ['Arabica', 'Robusta'])
-# coffee_roast_val = form.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
-# brewing_val = form.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
-# serving_type_val = form.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
-# milk_val = form.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
-# owncup_val = form.checkbox('Bring own cup')
-# form.form_submit_button('Submit')
+firstname = st.experimental_get_query_params()['firstname'][0]
+surname = st.experimental_get_query_params()['surname'][0]
 
-# st.markdown(f'''
-#   ☕ You have ordered:
-#   - Coffee bean: `{coffee_bean_val}`
-#   - Coffee roast: `{coffee_roast_val}`
-#   - Brewing: `{brewing_val}`
-#   - Serving type: `{serving_type_val}`
-#   - Milk: `{milk_val}`
-#   - Bring own cup: `{owncup_val}`
-#   ''')
+st.write(f'Hello **{firstname} {surname}**, how are you?')
