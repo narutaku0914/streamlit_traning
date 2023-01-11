4c8e5b851a4c4824271c4af89b974890633eeb6a
narutaku0914 <takumi.nalusawa@gmail.com>
2023-01-11 22:44:40 +0900

    Day25修正

--- work/app.py
+++ work/app.py
@@ -16,3 +16,23 @@ with col2:
 
 st.header('Output')
 st.write("st.session_state object:", st.session_state)
+
+st.title('Non st.session_state')
+
+kg=0
+lbs=0
+
+def lbs_to_kg():
+  kg =  lbs/2.2046
+def kg_to_lbs():
+  lbs = kg*2.2046
+
+st.header('Input')
+col1, spacer, col2 = st.columns([2,1,2])
+with col1:
+  pounds = st.number_input("Pounds:",on_change = lbs_to_kg)
+with col2:
+  kilogram = st.number_input("Kilograms:",on_change = kg_to_lbs)
+
+st.header('Output')
+st.write("st.session_state object:", [kg,lbs])
